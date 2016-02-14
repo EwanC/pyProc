49653097e3238f2d21afb4859aeb92cd454ff339
EwanC <ewan.cr@gmail.com>
2016-02-14 16:12:09 +0000

    move manager into its own file

---
+++ proc_directory.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from proc_stat import ProcStat
+from proc_swaps import ProcSwaps
+from proc_uptime import ProcUptime
+from proc_version import ProcVersion
+
+
+class ProcDirectory:
+    '''
+    Class manages the list of supported /proc
+    sub-files we have classes for.
+
+    As well as printing the information contained in
+    them to stdout.
+    '''
+
+    def __init__(self):
+        '''
+        Creates an instance of all the classes derived
+        from ProcBase. Representing abstractions of
+        the supported sub-files.
+        '''
+        self.file_wrappers = [ProcStat(),
+                              ProcSwaps(),
+                              ProcUptime(),
+                              ProcVersion()]
+
+    def dump_all(self):
+        '''
+        Iterates over all files and prints their details to
+        stdout using overridden procBase dump() function.
+        '''
+        for _file in self.file_wrappers:
+            _file.dump()  # Implemented in base class
--- pyProc.py
+++ pyProc.py
@@ -3,39 +3,7 @@
 import os
 import sys
 
-from proc_stat import ProcStat
-from proc_swaps import ProcSwaps
-from proc_uptime import ProcUptime
-from proc_version import ProcVersion
-
-
-class PrintManager:
-    '''
-    Class manages the list of supported /proc
-    sub-files we have classes for.
-
-    As well as printing the information contained in
-    them to stdout.
-    '''
-
-    def __init__(self):
-        '''
-        Creates an instance of all the classes derived
-        from ProcBase. Representing abstractions of
-        the supported sub-files.
-        '''
-        self.file_wrappers = [ProcStat(),
-                              ProcSwaps(),
-                              ProcUptime(),
-                              ProcVersion()]
-
-    def dump_all(self):
-        '''
-        Iterates over all files and prints their details to
-        stdout using overridden procBase dump() function.
-        '''
-        for _file in self.file_wrappers:
-            _file.dump()  # Implemented in base class
+from proc_directory import ProcDirectory
 
 
 if __name__ == '__main__':
@@ -45,5 +13,5 @@ if __name__ == '__main__':
         sys.exit()
 
     # Print all details
-    printer = PrintManager()
-    printer.dump_all()
+    proc_dir = ProcDirectory()
+    proc_dir.dump_all()
